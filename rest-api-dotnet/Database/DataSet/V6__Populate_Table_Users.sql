678b3fc5c9e410330f0bf6123e3477896c40e15a
Guilherme Fabris de Souza <guilherme.souza@sequor.com.br>
2025-04-28 16:22:40 -0300

    feat: add migrations create table users, populate table users and alter table person

---
+++ rest-api-dotnet/Database/DataSet/V6__Populate_Table_Users.sql
@@ -0,0 +1,8 @@
+SET NAMES utf8mb4;
+
+INSERT INTO `user` (`user_name`, `password`, `full_name`, `refresh_token`, `refresh_token_expiry_time`) VALUES
+('guilherme', 
+'240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9', 
+'GUILHERME FABRIS DE SOUZA', 
+'', 
+'2000-01-01 00:00:00');
