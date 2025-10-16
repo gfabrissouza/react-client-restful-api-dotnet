0c5f94faf71c0f2a89bec29401843aa80e1a14be
Guilherme Fabris de Souza <guilherme.souza@sequor.com.br>
2025-10-16 15:19:03 -0300

    feat: add value objects

---
+++ backend/rest-api-dotnet/Data/VO/BookVO.cs
@@ -0,0 +1,15 @@
+using RestApiDotNet.Hypermedia.Abstract;
+using RestApiDotNet.HyperMedia;
+
+namespace RestApiDotNet.Model
+{
+    public class BookVO : ISupportsHyperMedia
+    {
+        public long Id { get; set; }
+        public string Author { get; set; }
+        public DateTime LaunchDate { get; set; }
+        public decimal Price { get; set; }
+        public string Title { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
---
+++ backend/rest-api-dotnet/Data/VO/FileDetailVO.cs
@@ -0,0 +1,9 @@
+﻿namespace RestApiDotNet.Data.VO
+{
+    public class FileDetailVO
+    {
+        public string DocumentName { get; set; }
+        public string DocumentType { get; set; }
+        public string DocumentUrl { get; set; }
+    }
+}
